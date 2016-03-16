2974c3812d6ac7df8c0ae02eacab43cf98d725dd
KevinBozic <bozic.kevin@gmail.com>
2016-03-16 11:41:26 -0400

    Add upload functionality

---
+++ src/main/java/com/theironyard/services/AnonFileRepository.java
@@ -0,0 +1,7 @@
+package com.theironyard.services;//Created by KevinBozic on 3/16/16.
+
+import com.theironyard.entities.AnonFile;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnonFileRepository extends CrudRepository<AnonFile, Integer> {
+}
